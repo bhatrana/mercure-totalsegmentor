ce9a4032ea19c51eb03da5cbfa74f28015057d52
Raj Kumar Ranabhat <rajkumar.ranabhat@sunnybrook.ca>
2024-10-23 15:00:47 -0400

    minor correction bash

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 set -Eeo pipefail
 echo "-- Starting TotalSegmentator..."
 conda run -n mercure-totalsegmentator python mercure-totalsegmentator -i $MERCURE_IN_DIR -o $MERCURE_OUT_DIR
